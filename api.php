d841ba68f48cdc4e38cf38e4c2f0e036df9f7ce3
TreexHD <mini87479@gmail.com>
2021-08-08 00:29:55 +0800

    fix remote addr ""

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://ipinsight.io/query?ip=$_SERVER["REMOTE_ADDR"]",
+  CURLOPT_URL => "https://ipinsight.io/query?ip=$_SERVER[REMOTE_ADDR]",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
